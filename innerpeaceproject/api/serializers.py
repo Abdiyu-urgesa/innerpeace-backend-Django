5f8f1a8daed5db875d320d1b34cbac4ec8a1c72e
Abdiyu Urgesa <abdiyu16668@gmail.com>
2023-08-24 11:11:11 +0300

    tewat lay

--- innerpeaceproject/api/serializers.py
+++ innerpeaceproject/api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework.serializers import StringRelatedField ,SlugRelatedField,ModelSerializer
 from .models import *
 
-
 class NoteSerializer(ModelSerializer):
     class Meta:
         model = Note
